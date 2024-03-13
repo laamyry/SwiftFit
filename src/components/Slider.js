4e1923a6f22847776b8a4863db64cb0872f94c01
laamyry <slamiri10@gmail.com>
2024-03-13 23:20:30 +0000

    add main

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,7 +4,7 @@ import {
   BsChevronCompactLeft,
   BsChevronCompactRight,
 } from "react-icons/bs";
-import {RxDotFilled} from "react-icons/rx"
+import { RxDotFilled } from "react-icons/rx";
 export default function Slider() {
   const slides = [
     {
@@ -32,13 +32,13 @@ export default function Slider() {
     setCurrentIndex(newIndex);
   };
   const nextSlide = () => {
-    const isLastSlide = currentIndex ===  slides.length -1;
+    const isLastSlide = currentIndex === slides.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex);
   };
-  const goToSlide = (slideIndex)=>{
-    setCurrentIndex(slideIndex)
-  }
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
   return (
     <div className="max-w-full h-[500px] w-full m-auto relative group">
       <div
@@ -47,19 +47,26 @@ export default function Slider() {
         }}
         className="w-full h-full bg-cover bg-center duration-500"
       ></div>
-      <div className="hidden group-hover:block absolute top-[50%] left-0 -translate-x-0 translate-y-[-50%] text-white cursor-pointer">
+      <div className="hidden group-hover:block absolute top-[50%] m-5 p-2 rounded-full left-0 -translate-x-0 translate-y-[-50%] text-white cursor-pointer bg-black/20">
         <BsChevronCompactLeft
           onClick={prevSlide}
           size={30}
         />
       </div>
-      <div className="hidden group-hover:block absolute top-[50%] right-0 -translate-x-0 translate-y-[-50%] text-white cursor-pointer">
-        <BsChevronCompactRight onClick={nextSlide} size={30} />
+      <div className="hidden group-hover:block absolute top-[50%] m-5 p-2 rounded-full right-0 -translate-x-0 translate-y-[-50%] text-white cursor-pointer bg-black/20">
+        <BsChevronCompactRight
+          onClick={nextSlide}
+          size={30}
+        />
       </div>
-      <div className="flex justify-center top-4 py-2">
+      <div className="flex justify-center top-4 py-2 ">
         {slides.map((slide, slideIndex) => (
-          <div key={slideIndex} onClick={() => goToSlide(slideIndex)} className="text-2xl cursor-pointer">
-            <RxDotFilled/>
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className="text-2xl cursor-pointer"
+          >
+            <RxDotFilled />
           </div>
         ))}
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,38 @@
-
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Slider from "../components/Slider";
+import image from "../Assets/image.jpg";
 export default function Home() {
   return (
     <>
       <Header />
-      <Slider/>
+      <Slider />
+      <div className="container mt-10 flex pb-2">
+        <div className="image flex-1 text-center object-cover	overflow-hidden rounded-r-3xl">
+          <img src={image} alt="" />
+        </div>
+        <div className="flex flex-col justify-center text flex-1 pl-5 text-justify ">
+          <p className="title text-6xl font-black w-[90%] uppercase ">
+            Lorem ipsum dolor sit amet
+          </p>
+
+          <p className="content text-2xl w-[90%] leading-9 pt-2">
+            Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Totam est facere aliquid
+            mollitia quo quibusdam commodi accusamus id
+            laudantium, consequatur quasi, molestiae,
+            adipisci dolor illo omnis quisquam deserunt
+            nesciunt alias! Consectetur reprehenderit
+            perspiciatis qui magnam rem. Mollitia aspernatur
+            libero aliquam laudantium unde necessitatibus
+            dolores itaque dolorem laborum adipisci, cum
+            praesentium fuga, nihil nam eligendi
+            consequuntur hic error assumenda impedit
+            tempore?.
+          </p>
+        </div>
+      </div>
       <Footer />
     </>
-  )
+  );
 }
