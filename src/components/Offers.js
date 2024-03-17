c911eca01708799ff3935951dc52723f24f262a5
laamyry <slamiri10@gmail.com>
2024-03-17 07:11:12 +0000

    add banner

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -3,10 +3,10 @@ import banner from "../Assets/banner.png";
 export default function Offers() {
   return (
     <div>
-      <div className="h-80">
+      <div className="h-80 ">
         <img
-          className="h-80 w-full object-contain"
-          src={banner}   
+          className="h-80 flex w-full object-fill md:object-contain sm:object-none"
+          src={banner}
           alt=""
         />
       </div>
