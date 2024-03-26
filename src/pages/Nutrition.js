762ce6359683d71c17968a330ec569410762ca61
laamyry <slamiri10@gmail.com>
2024-03-26 03:11:23 +0000

    Reapply "update Nutrition.js"
    This reverts commit bbd534f3f676f7979c088076fa397da02d5af302.

--- src/pages/Nutrition.js
+++ src/pages/Nutrition.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import HEADER from "../components/Header.js";
-import FOOTER from "../components/Footer.js";
-export default function Nutrition() {
+import React from "react";
+import HEADER from "../components/Header";
+import FOOTER from "../components/Footer";
+import OFFERS from "../components/OffersNutri";
+import PRODUCTS from "../components/productsNutri";
+export default function Equipment() {
   return (
     <>
       <HEADER />
-      Nutrition
+      <OFFERS />
+      <PRODUCTS/>
       <FOOTER />
     </>
-  )
+  );
 }
