bbd534f3f676f7979c088076fa397da02d5af302
laamyry <slamiri10@gmail.com>
2024-03-26 03:02:33 +0000

    Revert "update Nutrition.js"
    This reverts commit 9d6c443603a73b2ff996222aff307e6127e1b939.

--- src/pages/Nutrition.js
+++ src/pages/Nutrition.js
@@ -1,15 +1,12 @@
-import React from "react";
-import HEADER from "../components/Header";
-import FOOTER from "../components/Footer";
-import OFFERS from "../components/OffersNutri";
-import PRODUCTS from "../components/productsNutri";
-export default function Equipment() {
+import React from 'react'
+import HEADER from "../components/Header.js";
+import FOOTER from "../components/Footer.js";
+export default function Nutrition() {
   return (
     <>
       <HEADER />
-      <OFFERS />
-      <PRODUCTS/>
+      Nutrition
       <FOOTER />
     </>
-  );
+  )
 }
