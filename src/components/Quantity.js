d82336203af8144fec3e7ba83eea427b0224a976
laamyry <slamiri10@gmail.com>
2024-03-27 00:09:05 +0000

    update quantity  file 👌

--- src/components/Quantity.js
+++ src/components/Quantity.js
@@ -19,10 +19,10 @@ export default function Quantity() {
           <button
             type="button"
             onClick={decrementQuantity}
-            className="flex-shrink-0 bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 inline-flex items-center justify-center border border-gray-300 rounded-md h-5 w-5 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
+            className="flex-shrink-0 bg-gray-100 hover:bg-gray-200 inline-flex items-center justify-center border border-gray-300 rounded-md h-5 w-5 focus:ring-gray-100 focus:ring-2 focus:outline-none"
           >
             <svg
-              className="w-2.5 h-2.5 text-gray-900 dark:text-white"
+              className="w-2.5 h-2.5 text-gray-900 "
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -40,7 +40,7 @@ export default function Quantity() {
           <input
             type="text"
             id="counter-input"
-            className="flex-shrink-0 text-gray-900 dark:text-white border-0 bg-transparent text-sm font-normal focus:outline-none focus:ring-0 max-w-[2.5rem] text-center"
+            className="flex-shrink-0 text-gray-900  border-0 bg-transparent text-sm font-normal focus:outline-none focus:ring-0 max-w-[2.5rem] text-center"
             placeholder=""
             value={quantity}
             onChange={(e) =>
@@ -51,10 +51,10 @@ export default function Quantity() {
           <button
             type="button"
             onClick={incrementQuantity}
-            className="flex-shrink-0 bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 inline-flex items-center justify-center border border-gray-300 rounded-md h-5 w-5 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
+            className="flex-shrink-0 bg-gray-100 hover:bg-gray-200 inline-flex items-center justify-center border border-gray-300 rounded-md h-5 w-5 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
           >
             <svg
-              className="w-2.5 h-2.5 text-gray-900 dark:text-white"
+              className="w-2.5 h-2.5 text-gray-900 "
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
